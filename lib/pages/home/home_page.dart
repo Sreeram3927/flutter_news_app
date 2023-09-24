7f967e22c982a5fb45872d1d090fe7db75265c25
Sreeram3927 <sreeram292004@gmail.com>
2023-09-24 18:54:25 +0530

    display trending news in a Carousel Slides

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,24 +1,50 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:news_watch/widgets/news_feed.dart';
 import 'package:news_watch/widgets/trending_news_feed.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final ScrollController horizontalController = ScrollController();
+  final ScrollController verticalController = ScrollController();
+
+  @override
+  void dispose() {
+    horizontalController.dispose();
+    verticalController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const [
-        TrendingNewsFeed(),
-        NewsFeed(),
-        NewsFeed(),
-        NewsFeed(),
-        NewsFeed(),
-        NewsFeed(),
-        NewsFeed(),
-        NewsFeed(),
-        NewsFeed(),
-        NewsFeed(),
+    return CustomScrollView(
+      controller: verticalController,
+      slivers: [
+        SliverToBoxAdapter(
+          child: CarouselSlider(
+            items: const [
+              TrendingNewsFeed(),
+              TrendingNewsFeed(),
+              TrendingNewsFeed(),
+              TrendingNewsFeed(),
+              TrendingNewsFeed(),
+            ],
+            options: CarouselOptions(
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 5),
+              aspectRatio: 2.0,
+              enlargeCenterPage: true,
+              enlargeStrategy: CenterPageEnlargeStrategy.height,
+            ),
+          )
+        ),
       ],
     );
   }
