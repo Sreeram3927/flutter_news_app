4f7ebddca1bfabb362d64fbea76ef4e72dabbf42
Sreeram3927 <sreeram292004@gmail.com>
2023-09-24 13:06:55 +0530

    add provider and time ago to news feed

--- lib/widgets/news_feed.dart
+++ lib/widgets/news_feed.dart
@@ -13,18 +13,37 @@ class NewsFeed extends StatelessWidget {
               padding: EdgeInsets.all(8.0),
               child: Column(
                 children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage('https://www.bing.com/th?id=ODF.KANCozAEZYO1NwXNST5YDQ&pid=news&w=16&h=16&c=14&rs=2&qlt=90&dpr=1.3'),
+                        radius: 7,
+                      ),
+                      SizedBox(width: 6),
+                      Text(
+                        "timesnownews",
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                      Text(
+                        " | 1h",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 6),
                   Text(
-                    'News Title',
+                    "How to Watch: NASA's OSIRIS-REx Mission Bringing Asteroid Sample Back to Earth After Travelling for 3 Years",
                     style: TextStyle(
-                      fontSize: 20,
+                      fontSize: 18,
                       fontWeight: FontWeight.bold,
                     ),
-                  ),
-                  Text(
-                    'News Description',
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
                   ),
                 ],
               ),
